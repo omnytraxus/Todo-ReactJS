b1fba9f5320be155b31b2510a80ab4a81674a5ef
omnitraxus <barbaros1585@gmail.com>
2021-07-07 01:15:00 +0500

    Подключил json-server. Подключил axios. Настроил добавление и удаление списков.

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -1,15 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import List from '../List';
 import Badge from '../Badge';
+
 import closeSvg from '../../assets/img/close.svg'
 
 import './AddList.scss';
 
 const AddList = ({ colors, onAdd }) => {
   const [visiblePopup, setVisiblePopup] = useState(false);
-  const [selectedColor, setSelectedColor] = useState(colors[0].id);
+  const [selectedColor, setSelectedColor] = useState(3);
+  const [isLoading, setisLoading] = useState(false);
   const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    if (Array.isArray(colors)) {
+      setSelectedColor(colors[0].id);
+    }
+  }, [colors]);
 
   const onClose = () => {
     setVisiblePopup(false);
@@ -22,13 +31,15 @@ const AddList = ({ colors, onAdd }) => {
       alert('Введите название списка');
       return;
     }
-    const color = colors.filter(c => c.id === selectedColor)[0].name;
-    onAdd({
-      "id": Math.random(),
-      "name": inputValue,
-      color
+    setisLoading(true)
+    axios.post('http://localhost:3001/lists', { "name": inputValue, colorId: selectedColor }).then(({ data }) => {
+      const color = colors.filter(c => c.id === selectedColor)[0].name;  
+      const listObj = { ...data, color: { name: color } };
+      onAdd(listObj);
+      onClose();
+    }).finally(() => {
+      setisLoading(false);
     });
-    onClose();
   }
 
   return (
@@ -40,10 +51,11 @@ const AddList = ({ colors, onAdd }) => {
         items={[
           {
             className: 'list__add-button',
-            icon: <svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-            </svg>,        
+            icon: 
+            (<svg width="12" height="12" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8 1V15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+              <path d="M1 8H15" stroke="black" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            </svg>),        
             name: 'Добавить список'
           }
         ]}
@@ -74,7 +86,9 @@ const AddList = ({ colors, onAdd }) => {
               />
             ))}
           </div>
-          <button onClick={addList} className="button">Добавить</button>
+          <button onClick={addList} className="button">
+            {isLoading ? 'Добавление...' : 'Добавить'}
+          </button>
         </div>
       )}
     </div>
